71c6a6ef5f2a55ac71b1e3da32d854d105b0882b
Joseph Ellis <josephellis95@gmail.com>
2024-04-25 11:27:28 -1000

    got server running 9000

--- api/server.js
+++ api/server.js
@@ -1,1 +1,17 @@
-// build your server here and require it from index.js
+const express = require("express")
+
+const server = express()
+
+server.use('*', (req, res, next) =>{
+ next({ status: 404, message: 'not found'})
+})
+
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+server.use(express.json())
+module.exports = server
