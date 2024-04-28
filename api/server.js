f2aeceace39db6c9436f87b683beb02316b38038
Joseph Ellis <josephellis95@gmail.com>
2024-04-28 09:22:27 -1000

    wip

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require("express")
 
+const carsRouter = require('./cars/cars-router') //this is a example do not use
+
 const server = express()
+server.use(express.json()); // if you dont have this, your screwed
+
+
+server.use('/api/cars', carsRouter)  //this is a example do not use
 
 server.use('*', (req, res, next) =>{
  next({ status: 404, message: 'not found'})
