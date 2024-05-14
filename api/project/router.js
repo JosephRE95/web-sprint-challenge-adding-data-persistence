b2fe348a5d8a8ec0c1b4201409df23552565704c
Joseph Ellis <josephellis95@gmail.com>
2024-05-14 14:49:39 -0500

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -8,7 +8,7 @@ const projectsModel = require('./model');
 router.post('/', async (req, res) => {
   try {
     const project = await projectsModel.createProject(req.body);
-    project.project_completed = project.project_completed === 1 
+    project.project_completed = !!project.project_completed
     res.status(201).json(project);
 
   } catch (error) {
