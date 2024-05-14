8b775bb223998623ec3006d86bc4beeb616ea52f
Joseph Ellis <josephellis95@gmail.com>
2024-05-14 18:39:35 -0500

    wip

--- api/task/model.js
+++ api/task/model.js
@@ -5,14 +5,14 @@ const db = require('../../data/dbConfig');
 function getAllTasks() {
   return db('tasks as t')
     .join('projects as p', 't.project_id', 'p.project_id')
-    .select('t.task_id', 't.task_description', 't.task_notes', 't.task_completed', 'p.project_name', 'p.project_description');
+    .select('t.*', 'p.project_name', 'p.project_description');
 }
 
 function getTaskById(id) {
   return db('tasks as t')
     .join('projects as p', 't.project_id', 'p.project_id')
     .where('t.task_id', id)
-    .select('t.task_id', 't.task_description', 't.task_notes', 't.task_completed', 'p.project_name', 'p.project_description')
+    .select('t.*', 'p.project_name', 'p.project_description')
     .first();
 }
 
